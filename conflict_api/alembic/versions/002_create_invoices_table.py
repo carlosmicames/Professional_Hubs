70ae8d7ca1097e3ad43ece50672b581d5019c1b0
Carlos Micames <cj.micames@gmai.com>
2026-01-15 13:39:24 -0400

    Fix migration chain

--- conflict_api/alembic/versions/002_create_invoices_table.py
+++ conflict_api/alembic/versions/002_create_invoices_table.py
@@ -11,8 +11,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '003'
-down_revision: Union[str, None] = '002'
+revision: str = '002'
+down_revision: Union[str, None] = '001'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- conflict_api/alembic/versions/004_add_billing_communication_logs.py
+++ conflict_api/alembic/versions/004_add_billing_communication_logs.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '004'
-down_revision: Union[str, None] = '003'
+down_revision: Union[str, None] = '002'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
